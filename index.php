235d00a8ac2782295ebf971b7402ee70cc372040
Dishon Polycap <dishonpolycap@gmail.com>
2023-03-05 17:33:31 +0300

    landing page

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Expert Finder</title>
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="./header.css">
+    <link rel="stylesheet" href="./footer.css">
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    
+        <section class="landing_section">
+            <div class="sect_info">
+                <article class="text">
+                <h2 class="section_title">Welcome to Expert Finder!</h2>
+                <div class="underline"></div>
+                <p>
+                    Expert Finder is web based service that securely connects clients with highly competent & qualified experts in nearly all fields.
+                </p>
+                </article>
+                <div class="sign_up_btn">
+                <a href="./register.php" class="reg_btn">register now</a>
+                </div>
+            </div>
+
+            <!--<div class="scroll_down">
+                <div class="scroll_img">
+                    <img src="icons/arrow-down-white.svg" alt="down" class="icon arrow-icon">
+                </div>
+                <p class="scroll_text">
+                    Scroll down
+                </p>
+            </div>-->
+        </section>
+
+        <?php include './footer.php'; ?>
+
+    <script src="./footer.js" type="text/javascript"></script>
+</body>
+</html>
