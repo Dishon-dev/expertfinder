7cbd74e70f5df3f56356ae38e13e1447e9a14cf4
Dishon Polycap <dishonpolycap@gmail.com>
2023-03-05 17:47:21 +0300

    sign out fun

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php 
+    session_start();
+
+    if(isset($_SESSION['name'])){
+
+        unset($_SESSION['name']);
+        session_destroy();
+
+    }
+
+    session_abort();
+
+    header("location: ../motorist-Homepage.php");
+?>
