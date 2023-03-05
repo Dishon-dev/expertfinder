894e7fef750b3e6ad93e06c4355483294414c82a
Dishon Polycap <dishonpolycap@gmail.com>
2023-03-05 17:39:57 +0300

    footer

---
+++ footer.php
@@ -0,0 +1,26 @@
+
+<footer class="footer">
+    <div class="footer_links">
+        <h3>Quick Links</h3>
+            <ul>
+                <li><a href="./index.php">Home</a></li><span>|</span>
+			    <li><a href="./experts.php">Our Experts</a></li><span>|</span>
+                <li><a href="">Experts Near Me</a></li><span>|</span>
+			    <li><a href="./contact.php">Contact Us</a></li><span>|</span>
+			    <li><a href="">Clients Views</a></li><span>|</span>
+                <li><a href="./login.php">Sign In</a></li><span>|</span>
+                <li><a href="./register.php">Sign Up</a></li>
+			</ul>
+    </div>
+       
+    <div class="socialmedia_links">
+        <h3>Follow Us</h3>
+            <a class="btn-social-icon" href="https://www.twitter.com/polycap_dishon"><i class="fab fa-twitter"></i></a>
+            <a class="btn-social-icon" href="https://www.linkedin.com/in/polycap-dishon/"><i class="fab fa-linkedin-in"></i></a>
+            <a class="btn-social-icon" href="https://github.com/dishon-dev"><i class="fab fa-github"></i></a>
+            <a class="btn-social-icon" href="https://www.facebook.com/dishon.polycapratumo"><i class="fab fa-facebook-f"></i></a>
+            <a class="btn-social-icon" href="https://instagram.com/_p.o.l.y_dishon"><i class="fab fa-instagram"></i></a>
+    </div>
+    <hr />
+    <p class="copyright"><?php echo App_Name; ?> &copy; <span class="get_year"></span>. All Rights Reserved</p>
+</footer>
